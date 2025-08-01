4c8842b38a52d9737963c7311e2687c206f02f96
DanielTrusca1 <marian.trusca@8x8.com>
2025-08-01 12:52:21 +0300

    Create Nav component and link to App

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 
 // Import UI components
+import Nav from "./components/Nav"
 import Home from './components/Home'
 import About from "./components/About"
 import Users from "./components/Users"
@@ -11,6 +12,9 @@ import Users from "./components/Users"
 function App() {
   return (
     <div className="App">
+
+      <Nav/>
+
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/about" element={<About/>}/>
