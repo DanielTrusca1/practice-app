4ae14490f2d3b4fbde29154a1159b9f7d0ce7ced
DanielTrusca1 <marian.trusca@8x8.com>
2025-08-01 15:37:49 +0300

    Add users mock data + programatically display users in list

---
+++ src/MockData.js
@@ -0,0 +1,15 @@
+// Mock users data
+export const users = [
+    {
+        name: "Alex",
+        age: 25,
+    },
+    {
+        name: "Daniel",
+        age: 20,
+    },
+    {
+        name: "Kate",
+        age: 23,
+    },
+]
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React from "react";
+
+import { users } from "../MockData";
 
 const Users = () => {
   return (
-    <div>
+    <div className="users">
       <h1>❯ Users</h1>
-      <p>
-        List of users: 
-      </p>
+      <p>List of users:</p>
+      {
+        // Display all users programatically
+        users.map((u, i) => {
+          return (
+            <div className="list-item" key={i}>
+              <p>Name: {u.name}</p>
+              <p>Age: {u.age}</p>
+            </div>
+          );
+        })
+      }
     </div>
-  )
-}
+  );
+};
 
-export default Users
+export default Users;
