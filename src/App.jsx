7c680c00f64b1e717682874c06f9cd172c244d71
DanielTrusca1 <marian.trusca@8x8.com>
2025-08-01 12:47:47 +0300

    Install React-Router library + create components directory and basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import './App.css';
 
+// React Router
+import { Routes, Route } from 'react-router-dom';
+
+// Import UI components
+import Home from './components/Home'
+import About from "./components/About"
+import Users from "./components/Users"
+
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/users" element={<Users/>}/>
+      </Routes>
     </div>
   );
 }
