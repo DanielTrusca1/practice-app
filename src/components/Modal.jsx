8a774dd13f507cf39ba97d5093da1cd655c07ee9
DanielTrusca1 <marian.trusca@8x8.com>
2025-08-04 11:23:59 +0300

    Add close functionality to the modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,15 +1,26 @@
-import React from "react";
-
 import { SmallButton } from "../reusable/SmallButton";
 
+import { useContext } from "react";
+import { ModalContext } from "../contexts/ContextModal";
+
 const Modal = () => {
+  // Modal Context State
+  const { setIsModalActive } = useContext(ModalContext);
+
+  // Handle Close
+  const handleClose = () => {
+    setIsModalActive(false);
+  };
+
   return (
     <div className="confirmation-modal">
       <p>You have unsaved changes to the user form.</p>
       <p>Are you sure you want to discard the changes?</p>
       <div className="row-flex-container">
-        <SmallButton>Cancel</SmallButton>
-        <SmallButton $variant="danger">Don't Save</SmallButton>
+        <SmallButton onClick={handleClose}>Cancel</SmallButton>
+        <SmallButton onClick={handleClose} $variant="danger">
+          Don't Save
+        </SmallButton>
       </div>
     </div>
   );
